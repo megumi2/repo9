1073ea612d06d0e393c44510267c6cd4a1707bf5
megumi <megumi.deai0820@gmail.com>
2019-04-14 16:45:40 +0900

    laravel18

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -3,7 +3,6 @@
 
 @section('content')
   <div class="container">
-    <div class="container">
       <div class="row">
         <div class="col-md-8 mx-auto">
           <h2>ニュース編集</h2>
@@ -15,6 +14,12 @@
               @endforeach
             </ul>
           @endif
+          <div class="form-group row">
+            <label class="col-md-2" for="title">タイトル</label>
+            <div class="col-md-10">
+              <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
+            </div>
+          </div>
           <div class="form-group row">
             <label class="col-md -2" for="body">本文</label>
             <div class="col-md-10">
@@ -43,6 +48,19 @@
     </div>
   </div>
 </form>
+<div class="row mt-5">
+  <div class="col-md-4 mx-auto">
+    <h2>編集履歴</h2>
+    <ul class="list-group">
+      @if ($news_form->histories != NULL)
+        @foreach ($news_form->histories as $history)
+          <li class="list-group-item">{{ $history->edited_at }}</li>
+        @endforeach
+      @endif
+    </ul>
+  </div>
+</div>
+</div>
 </div>
 </div>
 @endsection
